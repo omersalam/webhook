b4ef0fbc3b428a0ca26234e3e48bc36a260e470b
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-05 12:33:08 +0500

    up

--- index.js
+++ index.js
@@ -13,48 +13,6 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post('/webhook', function(req,res) {
-  console.log('Recieve a post request');
-  if(!req.body)  return res.sendStatus(400)
-    res.setHeader('Content-Type', 'application/json');
-  var city = req.body.queryResult.parameters['geo-city'];
-  var w = getWeather(city);
-  let response = " ";
-  let responseObj = {
-    "fulfillmentText": response,
-    "fulfillmentMessages":[{"text": {"text": [w]}}]
-    ,"source":""
-  }
-  return res.json(responseObj);
-  });
-
-var apiKey = '6628ad3fd90a97fb39ff9793c7569874';
-var result
-
-function cb (err, resposne, body){
-  if(err){
-    console.log('error');
-  }
-  var weather = JSON.parse(body)
-  if(weather.message === 'city not found'){
-    result = 'unable to get weather' + weather.message;
-  }
-  else
-  {
-    result = 'its' + weather.main.temp + 'degrees with' + weather.weather[0].description;
-  }
-}
-function getWeather(city) {
-  result = undefined;
-  var url = 'http://api.openweathermap.org/data/2.5/weather%20q=${city}&units=imperial&appid=${apiKey}';
-  var req = request(url, cb);
-  while(result == undefined){
-    require('deasync').runLoopOnce;
-  }
-  return result;
-}
-
-
 restService.post("/echo", function(req, res) {
   var speech =
     req.body.queryResult &&
