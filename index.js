220e66ec67e709d2554170c90bc872bc372d012b
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-09 23:05:14 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ restService.post('/webhook', function(req,res) {
     res.setHeader('Content-Type', 'application/json');
   var city = req.body.queryResult.parameters.City;
   var w = getWeather(city);
-  let response = "Hi";
+  let response = "H";
  //return res.JSON(JSON.stringify)({speech:w, displayText: w, source:"webhook-echo-sample"});
  // return res.json(responseObj);
     return res.json({
@@ -91,9 +91,9 @@ restService.post('/webhook', function(req,res) {
     fulfillmentMessages: [{"text": {"text": [w]}}],
     source: "webhook-echo-sample"
   });
-  });
+});
 
-let request = require('request');
+//let request = require('request');
 
 var result
 
@@ -114,7 +114,7 @@ function cb (err, resposne, body){
 function getWeather(city) {
   result = undefined;
   let apiKey = '6628ad3fd90a97fb39ff9793c7569874';
-  let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}';
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   request(url, cb );
   while(result === undefined){
     require('deasync').runLoopOnce();
