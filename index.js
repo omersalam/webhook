6bdf11615ecbd4d65a2f5af8ada4233406b32bd0
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-09 12:27:48 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ restService.post('/webhook', function(req,res) {
     ,"source":""
   }
   return res.json(responseObj);
-  });
+  })
 
-var apiKey = '6628ad3fd90a97fb39ff9793c7569874';
+//var apiKey = '6628ad3fd90a97fb39ff9793c7569874';
 var result
 
 function cb (err, resposne, body){
@@ -48,9 +48,9 @@ function cb (err, resposne, body){
     result = 'its' + weather.main.temp + 'degrees with' + weather.weather[0].description;
   }
 }
-function getWeather(city) {
+function getWeather(_city) {
   result = undefined;
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}';
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=6628ad3fd90a97fb39ff9793c7569874';
   var req = request(url, cb);
   while(result == undefined){
     require('deasync').runLoopOnce;
