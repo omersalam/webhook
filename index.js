3e14b7083410341b94a061cb83bdb164715fbdf1
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-09 15:59:05 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ restService.post('/webhook', function(req,res) {
 
 let request = require('request');
 
-var result
+var result;
 
 // function cb (err, resposne, body){
 //   if(err){
@@ -114,11 +114,15 @@ function getWeather(city) {
   result = undefined;
   let apiKey = '6628ad3fd90a97fb39ff9793c7569874';
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-  request(url, function (err, response, body) {
+  request(url, function (err, _response, body) {
     if(err){
       console.log('error:', error);
-    } else {
+    } if(weather.message === 'city not found'){
+      result = 'Unable to get weather' + weather.message;
+      }
+      else {
       let weather = JSON.parse(body)
+
       let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
       result = 'Right now its' + weather.main.temp + 'degrees with' + weather.weather[0].description
       console.log(message);
