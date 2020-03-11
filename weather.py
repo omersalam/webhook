beea39e5334d8a7596fd8308d4a201f7c1328db0
Omer Salam <45632199+omersalam@users.noreply.github.com>
2020-03-11 12:33:36 +0500

    Add files via upload

--- weather.py
+++ weather.py
@@ -48,12 +48,12 @@ def processRequest(req):
             speech = "Login Failed"
     elif phoneNumber == 'Fetch':
         url = 'http://20.46.150.26/iof/get_entities_list/?type_id=62&index_a=0&index_b=100'
-        request1 = requests.get(url, headers={'Authorization': 'Token e89f01f5d23dd9c2172e788ade9f0e363190b843'})
-        print(type(request1))
-        requestStatus = request1.json()
+        request2 = requests.get(url, headers={'Authorization': 'Token e89f01f5d23dd9c2172e788ade9f0e363190b843'})
+        print(type(request2))
+        requestStatus = request2.json()
         print(requestStatus['status'])
         if requestStatus['status'] == 200:
-            speech = "Welcome" + str(requestStatus['response'])
+            speech = "Welcome in fetch" + str(requestStatus['response'])
         else:
             speech = "Failed to fetech"
     else:
